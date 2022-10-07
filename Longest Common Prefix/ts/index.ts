d594f8c95567a84909b0f04586bacb4cf3b1d3c0
gabriel valera <hashiramakage@gmail.com>
2022-10-07 08:23:58 -0400

    save

---
+++ Longest Common Prefix/ts/index.ts
@@ -0,0 +1,16 @@
+function longestCommonPrefix(strs: string[]): string {
+
+    if(strs.length===0){
+        return ""
+    }
+    let prefijo:string = strs[0]
+    // for (let index = 1; index < array.length; index++) {
+    //     const element = array[index];
+        
+    // }
+
+    return ""
+};
+
+let lista:string[] = ["flower","flow","flight"]
+longestCommonPrefix(lista)
